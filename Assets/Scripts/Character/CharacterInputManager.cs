d5c798c538a7eed432bb63bf8b11f895e4917996
6ECCMEPTHIoIIJ <shamraev.alexandr.github@gmail.com>
2025-04-03 14:14:02 +0300

    fix character rotation glitch

--- Assets/Scripts/Character/CharacterInputManager.cs
+++ Assets/Scripts/Character/CharacterInputManager.cs
@@ -1,9 +1,15 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 public class CharacterInputManager : MonoBehaviour
 {
+    private bool _isWalking;
+    private bool _isStopping;
+
+    [SerializeField] private float stopDelay;
+
     public float WalkingDirection { get; private set; }
     public bool IsWalking => WalkingDirection != 0;
     public bool FlipX => WalkingDirection < 0;
@@ -11,6 +17,30 @@ public class CharacterInputManager : MonoBehaviour
 
     public void OnWalk(InputAction.CallbackContext context)
     {
-        WalkingDirection = context.ReadValue<float>();
+        var walkingDirection = context.ReadValue<float>();
+        _isWalking = walkingDirection != 0;
+        if (_isWalking)
+        {
+            WalkingDirection = walkingDirection;
+            if (_isStopping)
+            {
+                _isStopping = false;
+                StopAllCoroutines();
+            }
+        }
+        else if (!_isStopping)
+        {
+            _isStopping = true;
+            StartCoroutine(StopWalking());
+        }
+    }
+
+    private IEnumerator StopWalking()
+    {
+        yield return new WaitForSeconds(stopDelay);
+        if (!_isWalking)
+        {
+            WalkingDirection = 0;
+        }
     }
 }
